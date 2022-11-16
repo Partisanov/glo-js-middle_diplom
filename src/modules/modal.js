dacc7bea0d2b7be98c0c4a6b2bfb7e978b343fae
Konstantin Pozharitskiy <tripless@mail.ru>
2022-11-16 21:25:39 +0300

    add disable scroll on open modal window

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -5,46 +5,63 @@ export const modal = () => {
     const servicesModal = document.querySelector('.services-modal');
     const sertificateModal = document.querySelector('.sertificate__modal');
     const sertificateImg = document.querySelector('.sertificate__img');
+    const smoothBtn = document.querySelector('.smooth-scroll');
+
+
+    const disableScroll = () => {
+        const lockPaddingValue = window.innerWidth - body.offsetWidth + 'px';
+        overlay.style.display = 'block';
+        body.style.paddingRight = lockPaddingValue;
+        smoothBtn.style.opacity = '0';
+        body.classList.add('disable-scroll');
+    };
+
+    const enableScroll = () => {
+        overlay.style.display = 'none';
+        body.style.paddingRight = '0px';
+        smoothBtn.style.opacity = (window.scrollY >= (document.getElementById('offer').offsetHeight +
+            document.getElementById('offer').offsetTop)) ? '1' : '0';
+        body.classList.remove('disable-scroll');
+    };
 
     body.addEventListener('click', (e) => {
         //show callback modal
         if (e.target.closest('#btn-callback')) {
             e.preventDefault();
-            overlay.style.display = 'block';
+            disableScroll();
             callbackModal.style.display = 'block';
         }
         //close callback modal
         if ((e.target.classList.contains('overlay') ||
             e.target.classList.contains('header-modal__close')) && (callbackModal.style.display === 'block')) {
-            overlay.style.display = 'none';
+            enableScroll();
             callbackModal.style.display = 'none';
         }
         //show services modal
         if (e.target.closest('.service-button')) {
-            overlay.style.display = 'block';
+            disableScroll();
             servicesModal.style.display = 'block';
         }
         //close services modal
         if ((e.target.classList.contains('overlay') ||
             e.target.classList.contains('services-modal__close')) && (servicesModal.style.display === 'block')) {
-            overlay.style.display = 'none';
+            enableScroll();
             servicesModal.style.display = 'none';
         }
 
         //show image modal
         if (e.target.closest('.sertificate-document')) {
             e.preventDefault();
+            disableScroll();
             const imgSrc = e.target.closest('.sertificate-document').getAttribute('href');
             sertificateImg.setAttribute("src", imgSrc);
-            overlay.style.display = 'block';
             sertificateModal.style.display = 'block';
-
         }
 
         //close image modal
         if ((e.target.classList.contains('overlay') ||
             e.target.classList.contains('sertificate__btn-close')) && (sertificateModal.style.display === 'block')) {
-            overlay.style.display = 'none';
+            enableScroll();
             sertificateModal.style.display = 'none';
         }
     });
